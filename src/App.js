bc41ee29e41f4d07c6e72fd8d1eebf62d92c5a8e
Jingcheng Wang <jingwang49@gmail.com>
2023-10-25 12:28:46 +1100

    new build: installed google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {useState} from 'react';
 import {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [name] = useState("Jingcheng Wang");
+
   useEffect(() => {
     document.body.classList.add(
       'bg-slate-900',
@@ -20,7 +23,7 @@ function App() {
           <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
             <div>
               <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-                <a href="/">Jingcheng Wang</a>
+                <a href="/">{name}</a>
               </h1>
               <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
                 Marketing Manager at Australia Southworld Media
